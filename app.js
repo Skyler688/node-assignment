ceff8c56612c62cdbb4aa6e0de7c2e379d31c605
Skyler688 <skylergit86@gmail.com>
2025-04-03 12:07:28 -0600

    assignment done

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 require("dotenv").config();
 
+const cookieParser = require("cookie-parser");
+const MongoStore = require("connect-mongo");
+const methodOverride = require("method-override");
+const session = require("express-session");
+
 const expressLayouts = require("express-ejs-layouts");
 const express = require("express");
 const app = express();
@@ -10,6 +15,19 @@ connectDB();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(cookieParser());
+app.use(methodOverride("_method"));
+app.use(
+session({
+secret: "keyboard cat",
+resave: false,
+saveUninitialized: true,
+store: MongoStore.create({
+mongoUrl: process.env.MONGO_URI,
+}),
+})
+);
+
 
 app.use(expressLayouts);
 app.use(express.static("public"));
@@ -18,5 +36,7 @@ app.set("view engine", "ejs");
 
 
 app.use("/", require("./server/routes/main"));
+app.use("/", require("./server/routes/admin"));
+
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
